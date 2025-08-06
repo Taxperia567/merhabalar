47ca418c0aa7bbf4f349609a8825f5b911031377
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:33:26 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/mods', require('./routes/mods'));
+app.use('/api/license', require('./routes/license'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
