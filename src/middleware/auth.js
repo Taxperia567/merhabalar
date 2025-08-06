5cae6ecdbd5db8ea20411faf6f63844aa8128908
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:52:26 +0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
-
-module.exports = function (req, res, next) {
+module.exports = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1];
-  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+  if (!token) return res.status(401).json({ msg: 'Token yok, erişim reddedildi' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
+  } catch {
+    res.status(401).json({ msg: 'Geçersiz token' });
   }
 };
