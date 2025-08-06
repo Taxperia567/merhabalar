3a79d7d898d5205b00dccedf0ea4c7df6b72392f
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:48:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-
 const app = express();
 connectDB();
 
+// Body parser
 app.use(express.json());
+
+// Static uploads
+app.use('/uploads', express.static('uploads'));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/mods', require('./routes/mods'));
 app.use('/api/license', require('./routes/license'));
+app.use('/api/admin', require('./routes/admin'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server ${PORT} portunda çalışıyor`));
