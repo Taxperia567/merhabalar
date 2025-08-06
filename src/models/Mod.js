b87f1ce6cd1cf618af396d7ae4ae7524709630ab
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:50:55 +0300

    Update Mod.js

--- src/models/Mod.js
+++ src/models/Mod.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
 const ModSchema = new mongoose.Schema({
-  name: String,
-  version: String,
-  description: String,
-  fileUrl: String,
-  createdAt: { type: Date, default: Date.now }
+  name:          { type: String, required: true },
+  description:   { type: String },
+  version:       { type: String },
+  downloadUrl:   { type: String },          // örn: "/uploads/abcdef.zip"
+  allowedUsers:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  isPublic:      { type: Boolean, default: false },
+  createdAt:     { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Mod', ModSchema);
