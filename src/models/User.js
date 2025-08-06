78eab34089957d5e7cf5b86fadb4434d77142cc2
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:50:41 +0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  roles: { type: [String], default: ['user'] },
-  createdAt: { type: Date, default: Date.now }
+  email:      { type: String, required: true, unique: true },
+  password:   { type: String, required: true },
+  isAdmin:    { type: Boolean, default: false },
+  createdAt:  { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('User', UserSchema);
