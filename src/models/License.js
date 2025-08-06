9a21c34830ab9bb60ed69ceec32e8c993cb53a8f
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:51:57 +0300

    Update License.js

--- src/models/License.js
+++ src/models/License.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-
 const LicenseSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  mod: { type: mongoose.Schema.Types.ObjectId, ref: 'Mod' },
-  code: String,
+  user:        { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  mod:         { type: mongoose.Schema.Types.ObjectId, ref: 'Mod', required: true },
   activatedAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('License', LicenseSchema);
