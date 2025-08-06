8a0da8faed1c1ce6999ae483bc58d1e228f91d6c
Tamer <56173603+Taxperia567@users.noreply.github.com>
2025-08-07 02:53:42 +0300

    Update licenseController.js

--- src/controllers/licenseController.js
+++ src/controllers/licenseController.js
@@ -1,16 +1,29 @@
+const LicenseCode = require('../models/LicenseCode');
 const License = require('../models/License');
-const Mod = require('../models/Mod');
 
 exports.activate = async (req, res) => {
-  // TODO: check code validity, link to user
-  res.status(501).json({ msg: 'Not implemented' });
+  const { code } = req.body;
+  try {
+    const lc = await LicenseCode.findOne({ code });
+    if (!lc) return res.status(404).json({ msg: 'Kod bulunamadı' });
+    if (lc.usedBy) return res.status(400).json({ msg: 'Kod zaten kullanılmış' });
+
+    lc.usedBy = req.user.id;
+    lc.usedAt = Date.now();
+    await lc.save();
+
+    await License.create({ user: req.user.id, mod: lc.mod });
+    res.json({ msg: 'Lisans aktif edildi' });
+  } catch (e) {
+    console.error(e); res.status(500).send('Sunucu hatası');
+  }
 };
 
 exports.status = async (req, res) => {
   try {
-    const licenses = await License.find({ user: req.user.id }).populate('mod');
-    res.json(licenses);
-  } catch (err) {
-    res.status(500).send('Server error');
+    const list = await License.find({ user: req.user.id }).populate('mod');
+    res.json(list);
+  } catch (e) {
+    console.error(e); res.status(500).send('Sunucu hatası');
   }
 };
